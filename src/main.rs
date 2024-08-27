b029d5f38f1f1d864302b83916bc28b41cf8c443
Serghei Mateas <serghei.mateas@nuacom.ie>
2024-08-27 10:18:52 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::{env, process};
+use minigrep::{run, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("query {:?}", config.query);
+    println!("filename {:?}", config.file);
+
+    if let Err(e) = run(config) {
+        println!("App error: {}", e);
+        process::exit(1);
+    }
+}
